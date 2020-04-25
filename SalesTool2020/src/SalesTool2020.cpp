77b398cb0ae59b8de142ae022fa26658738d7016
Nareth Im <narethi@yahoo.com>
2020-04-25 12:38:55 -0700

    Initial commit of project.

---
+++ SalesTool2020/src/SalesTool2020.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : SalesTool2020.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+void displayGreeting() {
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+}
+
+
+int main() {
+	displayGreeting();
+
+	return 0;
+}
